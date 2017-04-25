88e161a3ec44fca07438547e5156e74457e693f6
Bogdan Ostojic <alukobond93@gmail.com>
2017-04-25 15:13:44 +0200

    Push it To The Limit

---
+++ git-pushit.sh
@@ -0,0 +1,12 @@
+
+git push origin
+start "chrome.exe" https://www.youtube.com/watch?v=9D-QD_HIfjA&t=0m18s
+
+#Salt-N-Pepa - Push it
+#
+#https://www.youtube.com/watch?v=vCadcBR95oU
+
+#SCARFACE
+#
+#https://www.youtube.com/watch?v=9D-QD_HIfjA
+#alias pushit="git push origin;  https://www.youtube.com/watch?v=vCadcBR95oU;"
--- pushit.sh
+++
@@ -1,3 +0,0 @@
-
-git push origin
-start https://www.youtube.com/watch?v=vCadcBR95oU
