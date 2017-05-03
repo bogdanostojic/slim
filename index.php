ee5fffc0693d630ed0ef46f02623a58561c34b77
bogdanostojic <alukobond93@gmail.com>
2017-05-03 18:10:02 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+use \Interop\Container\ContainerInterface as ContainerInterface;
+require __DIR__ . '/../app/app.php';	// Dodajemo glavni fodler zbog sigurnoshih razloga jer index.php se samo salje korisniku
+$app->run();							//Pokrecemo aplikaciju preko ove funkcije.
